b612e1948227dc32e08608bdc1eddbb70c1bc915
watsonnsw <watsonnsw@gmail.com>
2024-12-07 14:48:27 -0800

    Day 7

---
+++ 2024/7/bridge-repair-code-part-1.py
@@ -0,0 +1,28 @@
+
+def main(problem_input: str) -> None:
+    # process input
+    equations = []
+    for line in problem_input.splitlines():
+        total, nums = line.split(":")
+        equations.append((int(total), list(map(int, nums.split()))))
+
+
+    # calculate result
+    result = 0
+    for total, nums in equations:
+        def test(index, cum):
+            if index == len(nums):
+                return cum == total
+            if cum > total:
+                return False
+            n = nums[index]
+            return test(index + 1, cum * n) or test(index + 1, cum + n)
+        if test(1, nums[0]):
+            result += total
+
+    # print result
+    print(result)
+
+if __name__ == "__main__":
+    with open("2024/7/bridge-repair-input", "r") as f:
+        main(f.read())
--- startup.py
+++ startup.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 import sys
-
+import shutil
 import requests
 from bs4 import BeautifulSoup
 
@@ -24,7 +24,7 @@ class Startup:
         self.day = sys.argv[1]
         self.session = requests.Session()
         self.session.cookies.set("session", SESSION_COOKIE)
-        self._create_directories_if_nonexistent()
+        self._create_directory_if_nonexistent()
 
     def get_question_info(self) -> None:
         response = self.session.get(f"https://adventofcode.com/{YEAR}/day/{self.day}")
@@ -34,26 +34,15 @@ class Startup:
         self.problem_name: str = title[title.find(":") + 2 : -4]
         self.problem_path_name = self.problem_name.lower().replace(" ", "-")
         self.part = len(soup.findAll("h2"))
-        self.same_input = SAME_INPUT_TEXT in response.text
         self.code_file_name = f"{YEAR}/{self.day}/{self.problem_path_name}-code-part-{self.part}.py"
+        self.input_file_name = f"{YEAR}/{self.day}/{self.problem_path_name}-input"
         print(f"You are on part {self.part} of question {self.problem_name} ({YEAR} day {self.day})")
 
     def set_up_workspace(self) -> None:
         self.get_question_info()
-        if not self.same_input:
+        if self.part == 1:
             self.download_and_store_input()
         self.create_and_open_code_file()
-
-    @property
-    def input_file_name(self) -> str:
-        base_file_name = f"{YEAR}/{self.day}/input/{self.problem_path_name}-input-part-{{part}}.txt"
-        if self.same_input:
-            for part in range(self.part - 1, -1, -1):
-                potential_input_file = base_file_name.format(part=part)
-                if os.path.exists(potential_input_file):
-                    return potential_input_file
-            raise ValueError("No previous input file found")
-        return base_file_name.format(part=self.part)
 
     @property
     def prior_code_file_name(self) -> str:
@@ -61,23 +50,10 @@ class Startup:
             raise ValueError("No previous part")
         return f"{YEAR}/{self.day}/{self.problem_path_name}-code-part-{self.part - 1}.py"
 
-    def _get_previous_input_processing_code(self) -> str:
-        lines = []
-        with open(file=self.prior_code_file_name, mode="r") as f:
-            copy = False
-            for line in f.readlines():
-                if PROCESS_INPUT_MARKER in line:
-                    copy = True
-                elif CALCULATE_RESULT_MARKER in line:
-                    break
-                elif copy:
-                    lines.append(line)
-        return "".join(lines)
-
-    def _create_directories_if_nonexistent(self) -> None:
-        if not os.path.isdir(f"{YEAR}/{self.day}/input"):
+    def _create_directory_if_nonexistent(self) -> None:
+        if not os.path.isdir(f"{YEAR}/{self.day}/"):
             print("Creating directory")
-            os.makedirs(f"{YEAR}/{self.day}/input")
+            os.makedirs(f"{YEAR}/{self.day}/")
 
     def download_and_store_input(self) -> None:
         response = self.session.get(f"https://adventofcode.com/{YEAR}/day/{self.day}/input")
@@ -92,18 +68,16 @@ class Startup:
                 print("Exiting.")
                 return
 
-        if self.same_input or (self.part > 1 and input(PROCESSING_QUERY) == "y"):
-            processing_code = self._get_previous_input_processing_code()
+        if self.part > 1:
+            shutil.copy(src=self.prior_code_file_name, dst=self.code_file_name)
         else:
-            processing_code = DEFAULT_INPUT_PROCESSING
-
-        with open(file=self.code_file_name, mode="w") as f:
-            f.write(
-                TEMPLATE.format(
-                    input_file_name=self.input_file_name,
-                    input_processing=processing_code,
+            with open(file=self.code_file_name, mode="w") as f:
+                f.write(
+                    TEMPLATE.format(
+                        input_file_name=self.input_file_name,
+                        input_processing=DEFAULT_INPUT_PROCESSING,
+                    )
                 )
-            )
         subprocess.run(OPEN_FILE + [self.code_file_name])
 
 
