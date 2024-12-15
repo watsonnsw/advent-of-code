581dfacb4e27df8e82e389888bea7e537580957c
watsonnsw <100438843+watsonnsw@users.noreply.github.com>
2024-12-14 22:24:22 -0800

    Loc tweaks

--- src/lib/grid.py
+++ src/lib/grid.py
@@ -1,12 +1,18 @@
-from collections import deque
-from typing import Self, SupportsIndex
+import math
+from typing import Iterator, Self
 
 
 class Loc(tuple):
     def __new__(cls, *args):
-        if len(args) == 1:
-            return super(Loc, cls).__new__(cls, (args[0][0], args[0][1]))
-        return super(Loc, cls).__new__(cls, (args[0], args[1]))
+        try:
+            if len(args) == 1:
+                x, y = args[0][0], args[0][1]
+            else:
+                x, y = args[0], args[1]
+            return super(Loc, cls).__new__(cls, (int(x), int(y)))
+        except:
+            print(f"Could not create Loc from args: {args}")
+            raise
 
     def rotate_clockwise(self) -> Self:
         return Loc(self[1], -self[0])
@@ -26,8 +32,23 @@ class Loc(tuple):
     def __ge__(self, other) -> bool:
         return self[0] >= other[0] or self[1] >= other[1]
 
-    def __mul__(self, value: int) -> tuple:
+    def __gt__(self, other) -> bool:
+        return self[0] > other[0] or self[1] > other[1]
+
+    def __mul__(self, value: int) -> Self:
         return Loc(self[0] * value, self[1] * value)
+
+    def __floordiv__(self, value: int) -> Self:
+        return Loc(self[0] // value, self[1] // value)
+
+    def __neg__(self) -> Self:
+        return Loc(-self[0], -self[1])
+
+    def manhattan_dist(self, other=(0, 0)) -> int:
+        return abs(self[0] - other[0]) + abs(self[1] - other[1])
+
+    def dist(self, other=(0, 0)) -> float:
+        return math.sqrt((self[0] - other[0]) ** 2 + (self[1] - other[1]) ** 2)
 
 
 class Dir:
@@ -60,13 +81,13 @@ class Grid(dict):
             for j, c in enumerate(line.strip()):
                 self[Loc(i, j)] = c
 
-    def find(self, target_value):
+    def find(self, target_value: str) -> Loc:
         for key, value in self.items():
             if value == target_value:
                 return key
         return None
 
-    def findall(self, target_value):
+    def findall(self, target_value) -> list[Loc]:
         keys = []
         for key, value in self.items():
             if value == target_value:
@@ -82,20 +103,20 @@ class Grid(dict):
             res.append("".join(line))
         return "\n".join(res)
 
-    def rows(self):
+    def rows(self) -> Iterator[int]:
         return range(self.m)
 
-    def cols(self):
+    def cols(self) -> Iterator[int]:
         return range(self.n)
 
-    def valueset(self):
+    def valueset(self) -> set[str]:
         result = set()
         for values in self.values():
             result |= set(values)
         return result
 
-    def get_row(self, row: int):
+    def get_row(self, row: int) -> list[str]:
         return [self[Loc(row, col)] for col in self.cols()]
 
-    def get_col(self, col: int):
+    def get_col(self, col: int) -> list[str]:
         return [self[Loc(row, col)] for row in self.rows()]
